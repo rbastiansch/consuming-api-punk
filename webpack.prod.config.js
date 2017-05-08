087ba427b7891c141507f7d53fce47bfb475fd67
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2017-05-08 17:44:15 -0300

    added new file to configuration to build app

---
+++ webpack.prod.config.js
@@ -0,0 +1,86 @@
+const { resolve } = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: resolve(__dirname, 'app'),
+
+  entry: [
+    'react-hot-loader/patch',
+    // activate HMR for React
+
+    'webpack-dev-server/client?http://localhost:8080',
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
+
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading
+    // only- means to only hot reload for successful updates
+
+    './App.js'
+    // the entry point of our app
+  ],
+  output: {
+    filename: 'bundle.js',
+    // the output bundle
+
+    path: resolve(__dirname, 'dist'),
+
+    publicPath: '/'
+    // necessary for HMR to know where to load the hot update chunks
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: resolve(__dirname, 'dist'),
+    // match the output path
+
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [ 'babel-loader' ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+
+    new webpack.optimize.UglifyJsPlugin(),
+
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+
+  ],
+  externals: {
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true
+  },
+};
