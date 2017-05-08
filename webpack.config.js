8720dc4ad478251a235867e2a6236ec9c7b32708
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2017-05-07 22:44:39 -0300

    added postcss-loader on module

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,15 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader?modules', ],
+        use: [ 'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ],
       },
     ],
   },
